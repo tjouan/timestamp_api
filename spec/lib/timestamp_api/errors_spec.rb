9fea8ffeeafcb9835b93e438e76d0ebefe07d62e
Michael Baudino <michael.baudino@alpine-lab.com>
2016-01-24 13:11:41 +0400

    Enhance tests on error class for code coverage vanity 100%

--- spec/lib/timestamp_api/errors_spec.rb
+++ spec/lib/timestamp_api/errors_spec.rb
@@ -3,11 +3,15 @@ require "spec_helper"
 describe TimestampAPI::MissingAPIKey do
   it "defines a custom message" do
     expect(TimestampAPI::MissingAPIKey.instance_methods(false)).to include :message
+    expect(TimestampAPI::MissingAPIKey.new.message).to_not be_nil
+    expect(TimestampAPI::MissingAPIKey.new.message).to_not be_empty
   end
 end
 
 describe TimestampAPI::InvalidServerResponse do
   it "defines a custom message" do
     expect(TimestampAPI::InvalidServerResponse.instance_methods(false)).to include :message
+    expect(TimestampAPI::InvalidServerResponse.new.message).to_not be_nil
+    expect(TimestampAPI::InvalidServerResponse.new.message).to_not be_empty
   end
 end
