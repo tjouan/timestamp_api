59aa127a561b35159711bbb20260126a5624445d
Michael Baudino <michael.baudino@alpine-lab.com>
2016-01-31 20:26:31 +0400

    Fix a bug where camelization did not properly force first letter to be lowercase

--- lib/timestamp_api/utils.rb
+++ lib/timestamp_api/utils.rb
@@ -1,7 +1,7 @@
 module TimestampAPI
   module Utils
     def camelize(symbol)
-      symbol.to_s.split('_').each_with_index.map{ |chunk, idx| idx == 0 ? chunk : chunk.capitalize }.join
+      symbol.to_s.split('_').each_with_index.map{ |chunk, idx| idx == 0 ? chunk : chunk.capitalize }.join.gsub(/^\w/, &:downcase)
     end
   end
 end
