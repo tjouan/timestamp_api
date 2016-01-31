15513a7e331816d187a5171790bd9651e628d8db
Michael Baudino <michael.baudino@alpine-lab.com>
2016-01-31 19:12:29 +0400

    Remove :account_id attributes from models, as they're not supposed to be usefully exposed

--- lib/timestamp_api/models/client.rb
+++ lib/timestamp_api/models/client.rb
@@ -2,6 +2,6 @@ module TimestampAPI
   class Client < Model
     api_path "/clients"
 
-    has_attributes :id, :created_at, :updated_at, :account_id, :name, :code, :is_archived, :is_account_default
+    has_attributes :id, :created_at, :updated_at, :name, :code, :is_archived, :is_account_default
   end
 end
--- lib/timestamp_api/models/project.rb
+++ lib/timestamp_api/models/project.rb
@@ -2,7 +2,7 @@ module TimestampAPI
   class Project < Model
     api_path "/projects"
 
-    has_attributes :id, :created_at, :updated_at, :account_id, :name, :code, :color, :initiation_date,
+    has_attributes :id, :created_at, :updated_at, :name, :code, :color, :initiation_date,
                    :target_completion_date, :is_archived, :is_billable, :is_public, :is_approvable
 
     belongs_to :client
